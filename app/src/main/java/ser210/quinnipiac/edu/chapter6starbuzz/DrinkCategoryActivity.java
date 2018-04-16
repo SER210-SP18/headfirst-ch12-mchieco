3cdca33e4e5927f1c547de1f62813e9b3b266abe
mchieco <31969451+mchieco@users.noreply.github.com>
2018-04-16 01:40:59 -0400

    commit

--- app/src/main/java/ser210/quinnipiac/edu/chapter6starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/ser210/quinnipiac/edu/chapter6starbuzz/DrinkCategoryActivity.java
@@ -1,39 +1,64 @@
 package ser210.quinnipiac.edu.chapter6starbuzz;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import android.widget.SimpleCursorAdapter;
 
-public class DrinkCategoryActivity extends Activity {
+public class DrinkCategoryActivity extends ListActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink_category);
-        ArrayAdapter<Drink> listApapter = new ArrayAdapter<Drink>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks);
-        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listApapter);
+        ListView listDrinks = getListView();
 
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> listDrinks,
-                                    View itemView,
-                                    int position,
-                                    long id) {
-                if (position == 0) {
-                    Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
-                    intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
-                    startActivity(intent);
-                }
-            }
-        };
-        listDrinks.setOnItemClickListener(itemClickListener);
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    @Override
+    public void onListItemClick(ListView listView,
+                                View itemView,
+                                int position,
+                                long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
